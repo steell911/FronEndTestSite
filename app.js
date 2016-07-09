440f71a4515b83a3c0dc34edf6ccff14ab8adf4f
steell911 <petryaev.alexander@ya.ru>
2016-07-09 23:59:44 +0300

    Added the modal

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //--------------------------------------------------------
 
 $(document).ready(function() {
+
 	$('.js-like').on('click', function(event) {
 		event.preventDefault();
 
@@ -11,9 +12,29 @@ $(document).ready(function() {
 		.addClass('is-liked');
 	})
 
+	//--------------------------------------------------------
+	//		Add link
+	//--------------------------------------------------------
+
 	$('.js-add-link').on('click', function(event) {
 		event.preventDefault();
 
 		$('.js-form').toggleClass('is-visible');
+	})
+
+	//--------------------------------------------------------
+	//		Modal
+	//--------------------------------------------------------
+
+	$('.js-show-modal').on('click', function(event) {
+		event.preventDefault();
+
+		$('.js-modal').addClass('is-visible');
+		$('.js-modal-overlay').addClass('is-visible');
+	})
+
+	$('.js-modal-overlay').on('click', function(event) {
+		$('.js-modal').removeClass('is-visible');
+		$('.js-modal-overlay').removeClass('is-visible');
 	})
 })
